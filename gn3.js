106d70bd4c56f4535834ef330064283dfca9a007
na-x-ril <138718203+na-x-ril@users.noreply.github.com>
2024-07-26 23:59:29 +0700

    Add files via upload

---
+++ gn3.js
@@ -0,0 +1,37 @@
+const audioContext = new AudioContext();
+
+const videoElement = document.querySelector('video');
+const audioStream = audioContext.createMediaElementSource(videoElement);
+
+const inGain = audioContext.createGain();
+const biquadFilter = audioContext.createBiquadFilter();
+const outGain = audioContext.createGain();
+
+const equalizer = {
+    31: { frequency: 31, gain: 15 },
+    62: { frequency: 62, gain: 10 },
+    125: { frequency: 125, gain: 5 },
+    250: { frequency: 250, gain: 0 },
+    500: { frequency: 500, gain: -5 },
+    1000: { frequency: 1000, gain: -5 },
+    2000: { frequency: 2000, gain: 0 },
+    4000: { frequency: 4000, gain: 5 },
+    8000: { frequency: 8000, gain: 10 },
+    16000: { frequency: 16000, gain: 1 }
+};
+
+for (const frequency in equalizer) {
+    const filterSettings = equalizer[frequency];
+    biquadFilter.frequency.value = filterSettings.frequency;
+    biquadFilter.Q.value = 1.4;
+    biquadFilter.gain.value = filterSettings.gain;
+}
+
+inGain.gain.value = 0.6;
+outGain.gain.value = 3.0;
+
+audioStream.connect(inGain);
+inGain.connect(biquadFilter);
+biquadFilter.connect(outGain);
+outGain.connect(audioContext.destination);
+console.log('Bass Boosted!');
